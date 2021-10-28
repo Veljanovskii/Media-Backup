776e5ec52bdd6f1d12feb2e2a49852aad081975f
Milos Veljanovski <milosveljanovski@elfak.rs>
2021-10-28 23:14:19 +0200

    Add project files.

---
+++ Media Backup/HelperClass.cs
@@ -0,0 +1,18 @@
+﻿using MediaDevices;
+using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Text;
+
+namespace Media_Backup
+{
+    public class HelperClass
+    {
+        public IEnumerable<MediaDevice> GetDevices()
+        {            
+            return MediaDevice.GetDevices();
+        }
+    }
+}
---
+++ Media Backup/MainForm.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Media_Backup;
+
+namespace Media_Backup
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            var proxy = new HelperClass();
+
+            var devices = proxy.GetDevices();
+            if (devices.Count() == 0)
+            {
+                var result = MessageBox.Show("No devices detected. Please plug in your device and try again.", "No active devices", MessageBoxButtons.RetryCancel);
+                if (result == DialogResult.Cancel)
+                {
+                    Application.Exit();
+                }
+                else if (result == DialogResult.Retry)
+                {
+                    Application.Restart();
+                }
+            }
+            else
+            {
+
+            }
+        }
+    }
+}
